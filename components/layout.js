39c0c89c656a5f214dfdeb7223767e963766c725
Süleyman GÜNDÜZ <suleyman.gunduz@bilyoner.com>
2020-10-23 22:41:19 +0300

    Meta content modified.

--- components/layout.js
+++ components/layout.js
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Contact info and my work experiences."
         />
         <meta
           property="og:image"
