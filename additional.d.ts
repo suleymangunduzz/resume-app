002fda569947fd9548cd4302df5c045aa7aa6e39
Suleyman GUNDUZ <suleymangunduzz@gmail.com>
2023-03-13 20:44:09 +0100

    add ts support

---
+++ additional.d.ts
@@ -0,0 +1,3 @@
+declare var __BASE_API_URL__:
+  | 'http://localhost:3001'
+  | 'https://resume-app-backend.vercel.app/';
--- components/comment.tsx
+++ components/comment.tsx
@@ -1,3 +1,4 @@
+import { FC } from 'react';
 import styles from './comment.module.css';
 import utilsStyles from '../styles/utils.module.css';
 
